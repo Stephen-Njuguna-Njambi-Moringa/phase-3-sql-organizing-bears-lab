f432379d8fae4e14027e6c7df3a0ec3eed6d3ba1
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-10-31 13:01:07 +0300

    Completed the Lab

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO bears (ID,NAME,AGE,SEX,COLOR,TEMPERAMENT,ALIVE)VALUES (1, 'Mr.Chocolate', 13, 'm', "black", "introvert", true );
+
+INSERT INTO bears (ID,NAME,AGE,SEX,COLOR,TEMPERAMENT,ALIVE)VALUES (23, 'Rowdy', 7, 'm', "blue", "introvert", true );
+
+INSERT INTO bears (ID,NAME,AGE,SEX,COLOR,TEMPERAMENT,ALIVE)VALUES (35, 'Tabitha', 14, 'f', "black", "introvert", true );
+
+INSERT INTO bears (ID,NAME,AGE,SEX,COLOR,TEMPERAMENT,ALIVE)VALUES (49, 'Sergent Brown', 3, 'm', "black", "introvert", true );
+
+INSERT INTO bears (ID,NAME,AGE,SEX,COLOR,TEMPERAMENT,ALIVE)VALUES (52, 'Melisa', 2, 'f', "black", "introvert", true );
+
+INSERT INTO bears (ID,NAME,AGE,SEX,COLOR,TEMPERAMENT,ALIVE)VALUES (61, 'Grinch', 22, 'm', "black", "introvert", true );
+
+INSERT INTO bears (ID,NAME,AGE,SEX,COLOR,TEMPERAMENT,ALIVE)VALUES (77, 'Wendy', 25, 'f', "black", "introvert", true );
+
+INSERT INTO bears (ID,NAME,AGE,SEX,COLOR,TEMPERAMENT,ALIVE)VALUES (12, null, 32, 'm', "black", "introvert", true );
